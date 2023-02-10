0455dd6438fd510d784055e5301a261f1128211c
Zitong <71951467+zitongwu@users.noreply.github.com>
2023-02-09 21:55:23 -0800

    Update BuoyManager

---
+++ AVSimulator/Assets/Scripts/BuoyManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuoyManager : MonoBehaviour
+{
+    public GameObject BuoyPrefab;
+    public int BuoyNumber = 10;
+    public GameObject Terrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Transform tr = GetComponent<Transform>();
+        Vector2 terrainSize = Terrain.GetComponent<MeshGenerator>().GetSize();
+        float sideLength = Terrain.GetComponent<MeshGenerator>().GetSideLength();
+        float xSize = terrainSize[0];
+        float zSize = terrainSize[1];
+        float xMin = -xSize / 2;
+        float xMax = xSize / 2;
+        float zMin = -zSize / 2;
+        float zMax = zSize / 2;
+        for (int i = 0; i < BuoyNumber; i++)
+        {
+            GameObject BuoyInstance = Instantiate(BuoyPrefab, new Vector3(Random.Range(xMin, xMax), 0, Random.Range(zMin, zMax)) * sideLength, Quaternion.identity);
+            BuoyInstance.transform.parent = tr;
+        }
+    }
+}
