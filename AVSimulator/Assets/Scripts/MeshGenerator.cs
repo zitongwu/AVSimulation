afeb6a50692fe657a5d7f826bec33638ca87418f
Zitong <71951467+zitongwu@users.noreply.github.com>
2023-02-09 19:30:04 -0800

    Change MeshGenerator: create a plane

--- AVSimulator/Assets/Scripts/MeshGenerator.cs
+++ AVSimulator/Assets/Scripts/MeshGenerator.cs
@@ -9,40 +9,82 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
+    public float sideLength = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        CreateShape();
-
+        StartCoroutine(CreateShape());
     }
 
-    void CreateShape()
+    private void Update()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 0, 1),
-            new Vector3(1, 0, 0),
-            new Vector3(1, 0, 1),
-        };
+        UpdateMesh();
+    }
 
-        triangles = new int[]
+    IEnumerator CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            0, 1, 2,
-            1, 3, 2
-        };
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 20f;
+                vertices[i] = new Vector3(x * sideLength, y, z * sideLength);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                yield return new WaitForSeconds(0.01f);
+            }
+            vert++;
+        }
+
     }
 
     // Update is called once per frame
-    void Update()
+    void UpdateMesh()
     {
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
 
         mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
     }
 }
