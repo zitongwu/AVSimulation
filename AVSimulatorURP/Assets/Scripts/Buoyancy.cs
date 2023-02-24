31a811f02b0b35d2c124dd37eeaf2edc541912a4
Zitong <71951467+zitongwu@users.noreply.github.com>
2023-02-24 14:21:31 -0800

    Add Buoyancy script

---
+++ AVSimulatorURP/Assets/Scripts/Buoyancy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Buoyancy : MonoBehaviour
+{
+    public float underwaterDrag = 3f;
+    public float underwaterAngularDrag = 1f;
+    public float airDrag = 0f;
+    public float airAngularDrag = 0.05f;
+    public float floatingPower = 15f;
+    public float waterHeight = 0.493f;
+    Rigidbody m_RigidBody;
+
+    bool underwater;
+
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float difference = transform.position.y - waterHeight;
+        if (difference < 0)
+        {
+            m_RigidBody.AddForceAtPosition(Vector3.up * floatingPower * Mathf.Abs(difference), transform.position, ForceMode.Force);
+            if (!underwater)
+            {
+                underwater = true;
+                SwitchState(true);
+            }
+        }
+        else if (underwater)
+        {
+            underwater = false;
+            SwitchState(false);
+        }
+    }
+
+    void SwitchState(bool isUnderwater)
+    {
+        if (isUnderwater)
+        {
+            m_RigidBody.drag = underwaterDrag;
+            m_RigidBody.angularDrag = underwaterAngularDrag;
+        }
+        else
+        {
+            m_RigidBody.drag = airDrag;
+            m_RigidBody.angularDrag = airDrag;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
